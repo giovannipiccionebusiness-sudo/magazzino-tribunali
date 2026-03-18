3fc73e1cb7fdc05f8a220b5aea5c44b159c859a0
giovannipiccionebusiness-sudo <giovanni.piccione.business@gmail.com>
2026-03-18 15:11:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbykSNxJ0MEnYtzQpsmJcLGHZKgdVDz93NSaiGb9a-kWwp-IdREhgZsbVS_bTmVq92I/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbx94nYPyyDQdnNVugouXXgFF_VEWK5AHKIenX6CNbOS_yzGKbF-f-ZFN1QPIooow2qd/exec";
 
 let APP = {
   user: null,
